3351da62de235f0d7c211e321b5c7ecbe09ba6ff
TrondSpjelkavik <trond.spjelkavik@gmail.com>
2021-04-28 21:44:15 +0200

    added robots.txt

--- pages/templates.js
+++ pages/templates.js
@@ -7,6 +7,7 @@ function templates({ data }) {
       <Meta
         title="Din CV"
         font="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&display=swap"
+        description="Velg blant flere unike maler. Bygg din CV, se igjennom og skriv ut. Her kan du raskt og enkelt lage din egen CV."
       ></Meta>
       <Templates data={data}></Templates>
     </>
