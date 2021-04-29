190c2c478c65ade65bb7a01138ab4421e82dfcb8
TrondSpjelkavik <trond.spjelkavik@gmail.com>
2021-04-29 10:51:18 +0200

    added shaddow to template images

--- components/Templates.js
+++ components/Templates.js
@@ -73,6 +73,7 @@ const TemplateImage = styled.img`
   margin-top: 20px;
   cursor: pointer;
   overflow: hidden;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   &:hover {
     transition: 0.3s ease-in;
     transform: scale(1.04);
