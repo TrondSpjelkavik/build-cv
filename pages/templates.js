5e542651213bd6cee28953b5dec78d4cb92d70aa
TrondSpjelkavik <trond.spjelkavik@gmail.com>
2021-06-02 10:36:59 +0200

    templates update

--- pages/templates.js
+++ pages/templates.js
@@ -1,7 +1,7 @@
 import Templates from "../components/Templates";
 import Meta from "../components/Layout/Meta";
 
-function templates({ data }) {
+function templates() {
   return (
     <>
       <Meta
@@ -9,20 +9,9 @@ function templates({ data }) {
         font="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&display=swap"
         description="Velg blant flere unike maler. Bygg din CV, se igjennom og skriv ut. Her kan du raskt og enkelt lage din egen CV."
       ></Meta>
-      <Templates data={data}></Templates>
+      <Templates></Templates>
     </>
   );
 }
 
 export default templates;
-
-export async function getStaticProps() {
-  const res = await fetch("https://build-cv-api.appspot.com/api");
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
